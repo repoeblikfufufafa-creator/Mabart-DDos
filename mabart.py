29fa46b233c28b45221e01bc720cfa60d1617bcf
repoeblikfufufafa-creator <repoeblikfufufafa@gmail.com>
2026-01-06 22:17:59 +0700

    Update mabart.py

--- mabart.py
+++ mabart.py
@@ -35,8 +35,8 @@ while attemps < 100:
     username = input("\033[32mEnter your username: \033[0m")
     password = input("\033[31mEnter your password: \033[0m")
 
-    if username == 'bp4' and password == 'bp4':
-        print("\033[32m⟩⟩ Hai...! Welcome to zona attack BLACKPHANTER \033[0m")
+    if username == 'termul' and password == '0geb':
+        print("\033[32m⟩⟩ Welcome to zona attack for Free Palistine \033[0m")
         break
     else:
         print('Incorrect credentials. Check if you have Caps lock on and try again.')
